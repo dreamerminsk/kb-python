8d70eb173335d806c9983f74ad6ee163798f0af4
dreamerminsk <David70908019Miller>
2020-02-24 19:42:59 +0300

    years parser

--- years.py
+++ years.py
@@ -1,2 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+
+s = requests.Session()
+
+
+def get_page(ref):
+    try:
+        r = s.get(ref, timeout=24)
+        doc = BeautifulSoup(r.text, 'html.parser')
+        return doc, None
+    except Exception as ex:
+        return None, ex
