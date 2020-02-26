6ab59ff4271e2036e9b1615d713a6720ca8059f6
dreamerminsk <David70908019Miller>
2020-02-26 21:34:59 +0300

    parsing boxoffice

--- years.py
+++ years.py
@@ -35,10 +35,12 @@ def save_film(f):
 for row in rows[1:]:
     cells = row.select('td')
     film = {}
+    boxoffice = {}
     for index, cell in enumerate(cells):
         print(index, cell)
         if index == 0:
             print('\tpos: ', cell.text)
+            boxoffice['pos'] = cell.text
         if index == 1:
             print('\ttitle: ', cell.text)
             film['title'] = cell.text
@@ -46,19 +48,26 @@ for row in rows[1:]:
             film['page'] = cell.select_one('a')['href']
             print('\tname: ', cell.select_one('a')['name'])
             film['id'] = cell.select_one('a')['name']
+            boxoffice['film'] = cell.select_one('a')['name']
         if index == 2:
             print('\toriginal: ' + cell.text)
             film['original'] = cell.text
         if index == 3:
             print('\tdistributor: ' + cell.text)
+            boxoffice['distributor'] = cell.text
         if index == 4:
             print('\tscreens: ' + cell.text)
+            boxoffice['screens'] = cell.text
         if index == 5:
             print('\ttotalRur: ' + cell.text)
+            boxoffice['total_rur'] = cell.text
         if index == 6:
             print('\ttotalUsd: ' + cell.text)
+            boxoffice['total_usd'] = cell.text
         if index == 7:
             print('\tspectaculars: ' + cell.text)
+            boxoffice['spectaculars'] = cell.text
         if index == 8:
             print('\tdays: ' + cell.text)
+            boxoffice['days'] = cell.text
     save_film(film)
