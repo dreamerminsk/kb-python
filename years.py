360bb45456e200444ef3980996efb6fa494bed4b
dreamerminsk <David70908019Miller>
2020-02-26 16:45:23 +0300

    full parsing year boxoffice

--- years.py
+++ years.py
@@ -23,16 +23,21 @@ doc, err = get_page('http://kinobusiness.com/kassovye_sbory/films_year/')
 rows = doc.select('table.calendar_year tr')
 for row in rows[1:]:
     cells = row.select('td')
+    film = {}
     for index, cell in enumerate(cells):
         print(index, cell)
         if index == 0:
             print('\tpos: ', cell.text)
         if index == 1:
             print('\ttitle: ', cell.text)
+            film['title'] = cell.text
             print('\tpage: ', cell.select_one('a')['href'])
+            film['page'] = cell.select_one('a')['href']
             print('\tname: ', cell.select_one('a')['name'])
+            film['id'] = cell.select_one('a')['name']
         if index == 2:
             print('\toriginal: ' + cell.text)
+            film['original'] = cell.text
         if index == 3:
             print('\tdistributor: ' + cell.text)
         if index == 4:
@@ -45,3 +50,10 @@ for row in rows[1:]:
             print('\tspectaculars: ' + cell.text)
         if index == 8:
             print('\tdays: ' + cell.text)
+    try:
+        r = cursor.execute('INSERT INTO `kb-films`(id,title,original,page) VALUES(?,?,?,?)',
+                           [film['id'], film['title'], film['original'], film['page']])
+        conn.commit()
+        print(r)
+    except Exception as e:
+        print(e)
