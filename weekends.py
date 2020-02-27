8b043bc91d79c3edf57ecfc1472a1189fa4fadde
dreamerminsk <David70908019Miller>
2020-02-27 16:16:38 +0300

    parsing list of weekends

--- weekends.py
+++ weekends.py
@@ -1,6 +1,5 @@
 from kb import urls
 from net import get_page
-from store import conn
 
 doc, err = get_page(urls['weekends'])
 rows = doc.select('table.calendar_tbody year tr')
@@ -10,33 +9,11 @@ for row in rows:
     for index, cell in enumerate(cells):
         print(index, cell)
         if index == 0:
-            print('\tpos: ', cell.text)
-        if index == 1:
             print('\ttitle: ', cell.text)
-            film['title'] = cell.text
+            # film['title'] = cell.text
             print('\tpage: ', cell.select_one('a')['href'])
-            film['page'] = cell.select_one('a')['href']
-            print('\tname: ', cell.select_one('a')['name'])
-            film['id'] = cell.select_one('a')['name']
-        if index == 2:
-            print('\toriginal: ' + cell.text)
-            film['original'] = cell.text
-        if index == 3:
-            print('\tdistributor: ' + cell.text)
-        if index == 4:
-            print('\tscreens: ' + cell.text)
-        if index == 5:
+            # film['page'] = cell.select_one('a')['href']
+        if index == 1:
             print('\ttotalRur: ' + cell.text)
-        if index == 6:
-            print('\ttotalUsd: ' + cell.text)
-        if index == 7:
-            print('\tspectaculars: ' + cell.text)
-        if index == 8:
-            print('\tdays: ' + cell.text)
-    try:
-        r = conn.execute('INSERT INTO `kb-films`(id,title,original,page) VALUES(?,?,?,?)',
-                         [film['id'], film['title'], film['original'], film['page']])
-        conn.commit()
-        print(r)
-    except Exception as e:
-        print(e)
+        if index == 3:
+            print('\tfilms: ' + cell.text)
