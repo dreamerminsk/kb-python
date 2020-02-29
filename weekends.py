fa3c6424bca5df794d9baedd11c4c7b1af474dd7
dreamerminsk <tutmirage@gmail.com>
2020-02-29 13:58:29 +0300

    KeyError

--- weekends.py
+++ weekends.py
@@ -7,14 +7,12 @@ from net import get_page
 from store import save_weekend
 from utils import num
 
-doc, err = get_page(urls['weekends'])
-rows = doc.select('table.calendar_year tbody tr')
-
 
 def parse_weekend(week):
+    print(getweekend(week['weekend']))
     time.sleep(4)
     d, e = get_page(getweekend(week['weekend']))
-    rows = d.select('table.calendar_year tr')
+    rows = d.select('table#krestable tr')
 
     for row in rows[1:]:
         cells = row.select('td')
@@ -23,16 +21,16 @@ def parse_weekend(week):
         for index, cell in enumerate(cells):
             print(index, cell)
             if index == 1:
-                print('\tpos: ', cell.text)
                 boxoffice['pos'] = cell.text
+                print('\tpos: ', boxoffice['pos'])
             if index == 3:
                 print('\ttitle: ', cell.text)
                 film['title'] = cell.text
                 print('\tpage: ', cell.select_one('a')['href'])
                 film['page'] = cell.select_one('a')['href']
-                print('\tname: ', cell.select_one('a')['name'])
-                film['id'] = cell.select_one('a')['name']
-                boxoffice['film'] = cell.select_one('a')['name']
+                print('\tname: ', cell.select_one('a')['rel'])
+                film['id'] = cell.select_one('a')['rel']
+                boxoffice['film'] = cell.select_one('a')['rel']
             if index == 4:
                 print('\toriginal: ' + cell.text)
                 film['original'] = cell.text
@@ -58,6 +56,8 @@ def parse_weekend(week):
         # save_boxoffice(boxoffice)
 
 
+doc, err = get_page(urls['weekends'])
+rows = doc.select('table.calendar_year tbody tr')
 for row in rows:
     cells = row.select('td')
     weekend = {}
