2ee9529682e635ef3d4f4385b133d6f72d8cb5e8
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-03-02 01:24:02 +0300

    Update net.py
    Adding user-agent header

--- net.py
+++ net.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 s = requests.Session()
+s.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'})
 
 
 def get_page(ref):
