3bab27c4b3a370944847808f9eeddc8d64f0bd98
dreamerminsk <tutmirage@gmail.com>
2020-03-03 16:58:52 +0300

    simple refactoring

--- net.py
+++ net.py
@@ -13,9 +13,10 @@ s.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Ge
 def get_page(ref):
     logger.info(ref)
     try:
-        r = s.get(ref, timeout=24)
+        r = s.get(ref, timeout=32)
+        logger.info('{0} {1} {2}'.format(r.status_code, r.reason, r.elapsed))
         html = BeautifulSoup(r.text, 'html.parser')
         return html, None
     except Exception as ex:
-        print(ex)
+        logger.error(ex)
         return None, ex
