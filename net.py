34c209cdaaf527b821d8b787c535882eddc0ea1d
dreamerminsk <tutmirage@gmail.com>
2020-03-03 10:14:07 +0300

    simple refactoring

--- net.py
+++ net.py
@@ -1,11 +1,17 @@
+import logging.config
+
 import requests
 from bs4 import BeautifulSoup
+
+logging.config.fileConfig('log.conf')
+logger = logging.getLogger(__name__)
 
 s = requests.Session()
 s.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'})
 
 
 def get_page(ref):
+    logger.info(ref)
     try:
         r = s.get(ref, timeout=24)
         html = BeautifulSoup(r.text, 'html.parser')
