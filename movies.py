633c393916cb13ce4073908ccb57f9fecab5e3ca
dreamerminsk <tutmirage@gmail.com>
2020-03-03 22:38:49 +0300

    get_movie

--- movies.py
+++ movies.py
@@ -1,6 +1,8 @@
 from functional import seq
 
+from kb import get_movie
 from kb.models import Movie
+from net import get_page
 from store import get_films
 
 movies = get_films()
@@ -10,5 +12,9 @@ def pm(m):
     return Movie(m[0], m[1], m[2], m[3])
 
 
-movie = seq(movies).map(lambda m: pm(m)).first()
+def page(page):
+    return get_page(get_movie(page))
+
+
+movie = seq(movies).map(lambda m: pm(m)).map(lambda m: page(m.page)).first()
 print(movie)
